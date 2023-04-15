787058438f42b1bb97b28274ea5c5b423ae75194
Mostafaomer <87620409+Mostafaomer@users.noreply.github.com>
2023-04-15 14:32:27 +0200

    Add files via upload

---
+++ addp.js
@@ -0,0 +1,36 @@
+    let doctor;
+
+document.querySelectorAll(".dropdown-menu li a").forEach((e) => {
+    e.addEventListener('click', (ee) => {
+        doctor=ee.target.textContent;
+
+    })
+})
+document.querySelector('.save').addEventListener('click' , () => {
+
+        let code=document.querySelector(".code").value
+        let paid=document.querySelector(".paid").value
+        let toatl=document.querySelector(".total").value
+        let reason=document.querySelector(".reason").value
+        let phone=document.querySelector(".phone").value
+        let pn=document.querySelector(".pn").value
+        // console.log(code,paid,toatl,reason,phone,pn,doctor);
+        fetch("https://localhost:7299/AddPatient", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "DoctorName":doctor,
+                "PateintId":code,
+                "Phone":phone,
+                "paid":parseInt(paid),
+                "PatientName":"mj",
+                "Total":parseInt(toatl),
+                "Reason":"d"
+            })
+        }).then((e)=> {
+            window.location.href="index.html"
+        })
+    
+})
